09ae09ed48a07423f793b6a4396177ca978311cc
tycho <work.tycho+git@gmail.com>
2014-07-28 15:24:57 +0100

    Added XML parsing

--- MoodTune/MoodTune/Models/LastFMTagFetcher.vb
+++ MoodTune/MoodTune/Models/LastFMTagFetcher.vb
@@ -1,12 +1,18 @@
-﻿Public Class LastFMTagFetcher
+﻿Option Explicit On
+Option Infer On
+
+Public Class LastFMTagFetcher
 
     Private Shared APIKey As String = "2f56d6db848d93c852aa61d070fe1a9b"
 
     Public Shared Async Function GetSongs() As Threading.Tasks.Task(Of String)
         Dim songsfetcher As New Net.Http.HttpClient()
-        Dim response = Await songsfetcher.GetAsync("http://ws.audioscrobbler.com/2.0/?method=tag.search&tag=disco&api_key=" & APIKey)
-        Dim responsetext As String = ""
-        responsetext = Await response.Content.ReadAsStringAsync()
+        Dim response = Await songsfetcher.GetAsync("http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=disco&api_key=" & APIKey)
+        Dim responsetext = Await response.Content.ReadAsStringAsync()
+
+        Dim x As New IO.StringReader(responsetext)
+        Dim reader = System.Xml.XmlReader.Create(x)
+
         Return responsetext
     End Function
 End Class
