f1f8dc22669d2b1f9fe13d988f282336eca1ec99
tycho <work.tycho+git@gmail.com>
2014-07-28 13:55:23 +0100

    Fetching from lastFM, with API

--- MoodTune/MoodTune/Models/LastFMTagFetcher.vb
+++ MoodTune/MoodTune/Models/LastFMTagFetcher.vb
@@ -1,7 +1,10 @@
 ﻿Public Class LastFMTagFetcher
+
+    Private Shared APIKey As String = "2f56d6db848d93c852aa61d070fe1a9b"
+
     Public Shared Async Function GetSongs() As Threading.Tasks.Task(Of String)
         Dim songsfetcher As New Net.Http.HttpClient()
-        Dim response = Await songsfetcher.GetAsync("http://ws.audioscrobbler.com/2.0/?method=tag.search&tag=disco")
+        Dim response = Await songsfetcher.GetAsync("http://ws.audioscrobbler.com/2.0/?method=tag.search&tag=disco&api_key=" & APIKey)
         Dim responsetext As String = ""
         responsetext = Await response.Content.ReadAsStringAsync()
         Return responsetext
