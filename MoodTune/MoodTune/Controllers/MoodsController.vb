abd088a4aae6b030330fb930585a78d5137c4aa3
tycho <work.tycho+git@gmail.com>
2014-07-30 11:15:17 +0100

    added caching

--- MoodTune/MoodTune/Controllers/MoodsController.vb
+++ MoodTune/MoodTune/Controllers/MoodsController.vb
@@ -14,20 +14,25 @@ Namespace MoodTune
 
             Dim moodname As String = CStr(RouteData.Values("MoodName"))
 
-            Dim origsongs = Await LastFMTagFetcher.GetSongs(moodname)
-            Dim awaitarray As New List(Of Threading.Tasks.Task(Of String))
+            Dim cache = HttpRuntime.Cache
+            Dim cachedmood As String = CStr(cache("Moods." & moodname))
+            If cachedmood Is Nothing Then
+                Dim origsongs = (Await LastFMTagFetcher.GetSongs(moodname)).Take(10).ToList()
+                Dim awaitarray As New List(Of Threading.Tasks.Task(Of String))
 
-            For i = 0 To origsongs.Count - 1
-                awaitarray.Add(SoundCloudMapper.GetEmbedId(origsongs(i).Name))
-            Next
+                For i = 0 To origsongs.Count - 1
+                    awaitarray.Add(SoundCloudMapper.GetEmbedId(origsongs(i).Name))
+                Next
 
-            For i = 0 To origsongs.Count - 1
-                origsongs(i).Link = Await awaitarray(i)
-            Next
-            Dim songsJSON = JsonConvert.SerializeObject(origsongs)
-
+                For i = 0 To origsongs.Count - 1
+                    origsongs(i).Link = Await awaitarray(i)
+                Next
+                Dim songsJSON = JsonConvert.SerializeObject(origsongs)
+                cachedmood = songsJSON
+                cache("Moods." & moodname) = cachedmood
+            End If
             Dim result As New ContentResult
-            result.Content = songsJSON
+            result.Content = cachedmood
             Return result
         End Function
 
