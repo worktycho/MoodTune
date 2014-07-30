04433058db74cc47d1cb13c668c1fd2ececbd71c
louisdewar <Louis@Louiss-MacBook-Pro.local>
2014-07-30 14:22:40 +0100

    skiptrack

--- MoodTune/MoodTune/Views/Shared/skiptrack.js
+++ MoodTune/MoodTune/Views/Shared/skiptrack.js
@@ -1,1 +1,15 @@
-function 
+
+
+@Scripts.Render("~/scripts/SoundPlayer.js")
+
+
+function skipAnalyse(nametrack)
+{
+	nametrack = "SONGDATA_" + nametrack;
+	if (localStorage.getItem(nametrack)) {
+		localStorage.setItem(nametrack,(Number(localStorage.getItem(nametrack)) + 1));
+	} else {
+		localStorage.setItem(nametrack,1);
+	}
+	return localStorage.getItem(nametrack);
+}
